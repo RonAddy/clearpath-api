9813e56adeffbbe0988ac499786594999ded84e2
apearlcastellanos <100532072+apearlcastellanos@users.noreply.github.com>
2022-03-27 14:20:50 -0400

    Update publicBathrooms.ts

--- server/models/seed/publicBathrooms.ts
+++ server/models/seed/publicBathrooms.ts
@@ -22,5 +22,120 @@ long: -73.9770826,
 tags: ["Youth", "Education", "Accessible"]
 },
 ];
-
+[
+{
+name: "NYPL 58th ST",
+address: "127 East 58th Street New York, NY 10022",
+borough: "Manhattan",
+operationTime: "Tue-Sat 10 AM- 5PM",
+url: "https://www.nypl.org/locations/58th-street",
+email: "58stbranch@nypl.org",
+contact_number: "(212)-759-7358",
+lat: 40.7619724,
+long: -73.969306,
+tags: ["Partially Accessible" , " Toodler"]
+}
+];
+[
+{
+name: "NYPL 67th ST",
+address: "328 East 67th Street New York, NY 10065",
+borough: "Manhattan",
+operationTime: "Tue-Sat 11 AM-7PM",
+url: "https://www.nypl.org/locations/67th-street",
+email: "67stbranch@nypl.org",
+contact_number: "(212) 734-1717",
+lat: 40.7649454,
+long: -73.9595628,
+tags: ["Fully Accessible"]
+},
+];
+[
+{
+	 name: "NYPL 96th ST",
+	 address: "112 East 96th Street New York, NY 10128",
+	 borough: "Manhattan",
+	 operationTime: "Tue-Sat 10 AM-7PM",
+	 url: "https://www.nypl.org/locations/96th-street",
+	 email: "96stbranch@nypl.org",
+	 contact_number: "(212)-289-0908",
+	 lat: 40.7859686,
+	 long: -73.9518254,
+	 tags: ["Fully Accessible" , "Youth" , "Education"]
+},
+];
+[
+{
+	 name: "Aguilar Library",
+	 address: "174 East 110th Street New York, NY 10029",
+	 borough: "Bronx",
+	 operationTime: "Tue-Sat 10 AM-6PM",
+	 url: "https://www.nypl.org/locations/aguilar",
+	 email: "aguilar@nypl.org",
+	 contact_number: "(212)-534-2930",
+	 lat: 40.7942325,
+	 long: -73.943436,
+	 tags: ["Fully Acesible" , "Youth" , "Education"]
+},
+{
+	 name: "Allerton Library",
+	 address: "2740 Barnes Avenue Bronx, NY 10467",
+	 borough: "Bronx",
+	 operationTime: "Tue-Sat 10 AM-6PM",
+	 url: "https://www.nypl.org/locations/allerton",
+	 email: "allerton@nypl.org",
+	 contact_number: "(718)-881-4240",
+	 lat: 40.8668262,
+	 long: -73.863175,
+	 tags: ["Youth" , "Family Activities"]
+},
+{
+	 name: "Andrew Hesikeill",
+	 address: "40 West 20th Street New York, NY 10011",
+	 borough: "Manhattan",
+	 operationTime: "Tue-Sat 12 PM-7PM",
+	 url: "https://www.nypl.org/locations/heiskell",
+	 email: "talkingbooks@nypl.org",
+	 contact_number: "(212)-206-5400",
+	 lat: 40.7404616,
+	 long: -73.9932485,
+	 tags: ["Fully Accesible" , "Braille/Talking Services"]
+},
+{
+	 name: "Baterry Park City",
+	 address: "175 North End Avenue New York, NY 10282",
+	 borough: "Manhattan",
+	 operationTime: "Tue-Sat 10 AM- 6PM",
+	 url: "https://www.nypl.org/locations/battery-park-city",
+	 email: "batteryparkcity@nypl.org",
+	 contact_number: "(212)-790-3499",
+	 lat: 40.7156591,
+	 long: -74.0157365,
+	 tags: ["Fully Acessible" , "E-Books"]
+},
+{
+	 name: "Baychester library",
+	 address: "2049 Asch Loop North Bronx, NY 10475",
+	 borough: "Bronx",
+	 operationTime: "Tue-SAT 10 AM -6PM",
+	 url: "https://www.nypl.org/locations/baychester",
+	 email: "baychesterlibrary@nypl.org",
+	 contact_number: "718-379-6700",
+	 lat: 40.8707298,
+	 long: -73.8293366,
+	 tags: ["Fully Acessible" , "E-Books"]
+},
+{
+	 name: "Blomingdale Library",
+	 address: "150 West 100th Street New York, NY 10025",
+	 borough: "Manhattan",
+	 operationTime: "Tue-Sat 10 AM-5PM",
+	 url: "https://www.nypl.org/locations/bloomingdale",
+	 email: "bloomingdale@nypl.org",
+	 contact_number: "(212)-222-8030",
+	 lat: 40.795851,
+	 long: -73.9676214,
+	 tags: ["Fully Acesisible"]
+},
+];
 
