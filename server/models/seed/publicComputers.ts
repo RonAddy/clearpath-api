f7f78df9485dbeab77bd143181e3de9e66731754
apearlcastellanos <100532072+apearlcastellanos@users.noreply.github.com>
2022-04-23 00:26:02 -0400

    Update publicComputers.ts

--- server/models/seed/publicComputers.ts
+++ server/models/seed/publicComputers.ts
@@ -13,3 +13,59 @@ export default [
     tags: ["Youth", "Education", "Accessible"]
   },
 ];
+[
+{
+name: "NYPL 58th ST",
+  address: "127 East 58th Street New York, NY 10022",
+    borough: "Manhattan",
+    operationTime: "Tue-Sat 10 AM- 5PM",
+    url: "https://www.nypl.org/locations/58th-street",
+    email: "58st branch@nypl.org",
+    contact_number: "(212)-759-7358",
+    lat: 40.7619724,
+    long: -73.969306,
+    tags: ["Youth", "Education", "Accessible"]
+},
+  ];
+[
+{
+name: "NYPL 67th ST",
+  address: "328 East 67th Street New York, NY 10065",
+    borough: "Manhattan",
+    operationTime: "Tue-Sat 11 AM- 7PM",
+    url: "https://www.nypl.org/locations/67th-street",
+    email: "67st branch@nypl.org",
+    contact_number: "(212) 734-1717",
+    lat: 40.7649454,
+    long: -73.9595628,
+    tags: ["Youth", "Education", "Accessible"]
+},
+  ];
+[
+{
+name: "NYPL 96th ST",
+  address: "112 East 96th Street New York, NY 10128",
+    borough: "Manhattan",
+    operationTime: "Tue-Sat 10AM- 7PM",
+    url: "https://www.nypl.org/locations/96th-street",
+    email: "96st branch@nypl.org",
+    contact_number: "(212)-289-0908",
+    lat: 40.7859686,
+    long: -73.9518254,
+    tags: ["Youth", "Education", "Accessible"]
+},
+  ];
+[
+{
+name: "Aguilar Library",
+  address: "174 East 110th Street New York, NY 10029",
+    borough: "Bronx",
+    operationTime: "Tue-Sat 10AM- 6PM",
+    url: "https://www.nypl.org/locations/aguilar",
+    email: "aguilar@nypl.org",
+    contact_number: "(212)-534-2930",
+    lat: 40.7942325,
+    long: -73.943436,
+    tags: ["Youth", "Education", "Accessible"]
+},
+  ];
